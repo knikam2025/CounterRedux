d832c9e32ef94d0dc0cc3ceb31c02714113fe0ab
krishna <knikam2025@gmail.com>
2024-08-06 11:19:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
